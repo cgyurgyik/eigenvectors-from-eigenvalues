f497b76de152c4664c1f296872ce594c10cc49d3
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2019-11-14 17:17:20 -0500

    Update variable name.

--- GetEigenvectorFromEigenvalues.m
+++ GetEigenvectorFromEigenvalues.m
@@ -1,7 +1,7 @@
 % Calculating eigenvectors from eigenvalues.
 % For more information on proof, see: https://arxiv.org/pdf/1908.03795.pdf
 
-% Gets the normed eigenvalue ||v(i,j)||^2 from the matrix H.
+% Gets the normed eigenvector ||v(i,j)||^2 from the matrix H.
 % 'H' must be an N x N matrix.
 % 'i' is the row.    1 <= i <= N.
 % 'j' is the column. 1 <= j <= N.
@@ -15,19 +15,19 @@ function v_ij = getEigenvectorFromEigenvalues(H, i, j)
      hj(j, :) = []; % Remove jth row.
      hj_eigenvalues = eig(hj);
      
-     ith_eigenvalue = H_eigenvalues(i);
+     H_ith_eigenvalue = H_eigenvalues(i);
      
      % 3a. res_A = res_A * [λi(H) - λk(hj)] from k = 1 to (N - 1).
      res_A = 1;
      for k = 1:length(hj_eigenvalues)
-         res_A = res_A * (ith_eigenvalue - hj_eigenvalues(k));
+         res_A = res_A * (H_ith_eigenvalue - hj_eigenvalues(k));
      end
      
-     % 3b. res_B = res_B * [λi(H) - λk(H)]  from k = 1 to = N with limitation (k =/= i).
+     % 3b. res_B = res_B * [λi(H) - λk(H)]  from k = 1 to N with limitation (k =/= i).
      res_B = 1;
      for k = 1:length(H_eigenvalues)
-         if ith_eigenvalue ~= H_eigenvalues(k)
-             res_B = res_B * (ith_eigenvalue - H_eigenvalues(k));
+         if H_ith_eigenvalue ~= H_eigenvalues(k)
+             res_B = res_B * (H_ith_eigenvalue - H_eigenvalues(k));
          end
      end
      
