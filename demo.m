e350fa018a9f5561118bc336e64fb2d418533786
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2019-11-14 17:14:43 -0500

    Update demo.m

--- demo.m
+++ demo.m
@@ -1,36 +1,28 @@
-% Calculating eigenvectors from eigenvalues.
-% For more information on proof, see: https://arxiv.org/pdf/1908.03795.pdf
+% Demonstration of getEigenvectorsFromEigenvalues()
 
-% Gets the normed eigenvector ||v(i,j)||^2 from the matrix H.
-% 'H' must be an N x N matrix.
-% 'i' is the row.    1 <= i <= N.
-% 'j' is the column. 1 <= j <= N.
-function v_ij = getEigenvectorFromEigenvalues(H, i, j)
-     % 1. Calculate the eigenvalues of λi(H).
-     H_eigenvalues = eig(H);
-     
-     % 2. Calculate eigenvalues of hj 
-     hj = H;
-     hj(:, j) = []; % Remove jth column.
-     hj(j, :) = []; % Remove jth row.
-     hj_eigenvalues = eig(hj);
-     
-     ith_eigenvalue = H_eigenvalues(i);
-     
-     % 3a. res_A = res_A * [λi(H) - λk(hj)] from k = 1 to (N - 1).
-     res_A = 1;
-     for k = 1:length(hj_eigenvalues)
-         res_A = res_A * (ith_eigenvalue - hj_eigenvalues(k));
-     end
-     
-     % 3b. res_B = res_B * [λi(H) - λk(H)]  from k = 1 to N with limitation (k =/= i).
-     res_B = 1;
-     for k = 1:length(H_eigenvalues)
-         if ith_eigenvalue ~= H_eigenvalues(k)
-             res_B = res_B * (ith_eigenvalue - H_eigenvalues(k));
-         end
-     end
-     
-     % 4. ||v(i,j)||^2 = res_A / res_B.
-     v_ij = res_A/res_B;
-end
+H = [ -1  4  -2  ; ...
+      -3  4   0  ; ...
+      -3  1   3  ];
+
+[P, D] = eig(H); % Diagonalizable matrix such that H = P * D * P^-1
+disp('Using MatLab function: eig(H):')
+disp(P);
+disp(D); 
+
+v11 = getEigenvectorFromEigenvalues(H,1,1);
+v12 = getEigenvectorFromEigenvalues(H,1,2);
+v13 = getEigenvectorFromEigenvalues(H,1,3);
+
+v21 = getEigenvectorFromEigenvalues(H,2,1);
+v22 = getEigenvectorFromEigenvalues(H,2,2);
+v23 = getEigenvectorFromEigenvalues(H,2,3);
+
+v31 = getEigenvectorFromEigenvalues(H,3,1);
+v32 = getEigenvectorFromEigenvalues(H,3,2);
+v33 = getEigenvectorFromEigenvalues(H,3,3);
+ev_of_H = [v11 v21 v31; ...
+           v12 v22 v32; ...
+           v13 v23 v33];
+
+disp('getEigenvectorFromEigenvalues(H,i,j):')
+disp(ev_of_H);
