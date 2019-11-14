6ef74d67ab49d84f3f6d1e1ce40b097d7e7d26f3
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2019-11-14 16:21:13 -0500

    Create EVecFromEVal.m
    Demonstration.

---
+++ EVecFromEVal.m
@@ -0,0 +1,28 @@
+
+% Demonstration:
+H = [ -1  4  -2  ; ...
+      -3  4   0  ; ...
+      -3  1   3  ];
+
+[P, D] = eig(H);
+disp('Using MatLab function: eig(H):')
+disp(P);
+disp(D);
+
+v11 = getEigenvectorFromEigenvalues(H,1,1);
+v12 = getEigenvectorFromEigenvalues(H,1,2);
+v13 = getEigenvectorFromEigenvalues(H,1,3);
+
+v21 = getEigenvectorFromEigenvalues(H,2,1);
+v22 = getEigenvectorFromEigenvalues(H,2,2);
+v23 = getEigenvectorFromEigenvalues(H,2,3);
+
+v31 = getEigenvectorFromEigenvalues(H,3,1);
+v32 = getEigenvectorFromEigenvalues(H,3,2);
+v33 = getEigenvectorFromEigenvalues(H,3,3);
+ev_of_H = [v11 v21 v31; ...
+           v12 v22 v32; ...
+           v13 v23 v33];
+
+disp('getEigenvectorFromEigenvalues(H,i,j):')
+disp(ev_of_H);
