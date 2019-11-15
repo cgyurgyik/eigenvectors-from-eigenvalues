6be279e08814c16e2462b408678fe41c63ef7d19
ajt60gaibb <alex.townsend1987@gmail.com>
2019-11-15 12:00:18 -0500

    Fix Chris' code

--- GetEigenvectorFromEigenvalues.m
+++ GetEigenvectorFromEigenvalues.m
@@ -5,32 +5,24 @@
 % 'H' must be an N x N matrix.
 % 'i' is the row.    1 <= i <= N.
 % 'j' is the column. 1 <= j <= N.
-function v_ij = getEigenvectorFromEigenvalues(H, i, j)
+function v_ij = GetEigenvectorFromEigenvalues(H, i, j)
      % 1. Calculate the eigenvalues of λi(H).
      H_eigenvalues = eig(H);
+     ei = H_eigenvalues(i); 
      
      % 2. Calculate eigenvalues of hj 
      hj = H;
      hj(:, j) = []; % Remove jth column.
      hj(j, :) = []; % Remove jth row.
      hj_eigenvalues = eig(hj);
-     
-     H_ith_eigenvalue = H_eigenvalues(i);
-     
+          
      % 3a. res_A = res_A * [λi(H) - λk(hj)] from k = 1 to (N - 1).
-     res_A = 1;
-     for k = 1:length(hj_eigenvalues)
-         res_A = res_A * (H_ith_eigenvalue - hj_eigenvalues(k));
-     end
+     res_A = prod( ei - hj_eigenvalues );
      
      % 3b. res_B = res_B * [λi(H) - λk(H)]  from k = 1 to N with limitation (k =/= i).
-     res_B = 1;
-     for k = 1:length(H_eigenvalues)
-         if H_ith_eigenvalue ~= H_eigenvalues(k)
-             res_B = res_B * (H_ith_eigenvalue - H_eigenvalues(k));
-         end
-     end
+     H_eigenvalues(i) = []; 
+     res_B = prod( ei - H_eigenvalues );
      
-     % 4. ||v(i,j)||^2 = res_A / res_B.
+     % 4. |v(i,j)|^2 = res_A / res_B.
      v_ij = res_A/res_B;
 end
--- demo.m
+++ demo.m
@@ -1,27 +1,21 @@
 
-% Demonstration:
-H = [ -1  4  -2  ; ...
-      -3  4   0  ; ...
-      -3  1   3  ];
+% Demonstration: A must be Hermitian. s
+n = 3; 
+H = randn(n,n); 
+H = (H+H')/2; 
 
-[~, ~, eigenvectors] = eig(H);
+[eigenvectors, D] = eig(H);
 disp('Eigenvectors using MatLab function: eig(H):')
 disp(eigenvectors);
 
-v11 = getEigenvectorFromEigenvalues(H,1,1);
-v12 = getEigenvectorFromEigenvalues(H,1,2);
-v13 = getEigenvectorFromEigenvalues(H,1,3);
-
-v21 = getEigenvectorFromEigenvalues(H,2,1);
-v22 = getEigenvectorFromEigenvalues(H,2,2);
-v23 = getEigenvectorFromEigenvalues(H,2,3);
-
-v31 = getEigenvectorFromEigenvalues(H,3,1);
-v32 = getEigenvectorFromEigenvalues(H,3,2);
-v33 = getEigenvectorFromEigenvalues(H,3,3);
-ev_of_H = [v11 v21 v31; ...
-           v12 v22 v32; ...
-           v13 v23 v33];
+ev_of_H = zeros(n,n); 
+for i = 1:n
+    for j = 1:n
+        ev_of_H(i,j) = GetEigenvectorFromEigenvalues(H,i,j);
+    end
+end
 
 disp('Eigenvectors using getEigenvectorFromEigenvalues(H,i,j):')
 disp(ev_of_H);
+
+eigenvectors.^2 - ev_of_H'
