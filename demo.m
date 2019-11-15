1675ea00c8dcd035f091eff05c88ebd89a614733
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2019-11-15 18:42:20 -0500

    Update demo.m

--- demo.m
+++ demo.m
@@ -1,21 +1,16 @@
-
-% Demonstration: A must be Hermitian. s
+% Demonstration: H must be Hermitian.
 n = 3; 
 H = randn(n,n); 
 H = (H+H')/2; 
 
-[eigenvectors, D] = eig(H);
+[eigenvectors, ~] = eig(H);
 disp('Eigenvectors using MatLab function: eig(H):')
 disp(eigenvectors);
 
-ev_of_H = zeros(n,n); 
-for i = 1:n
-    for j = 1:n
-        ev_of_H(i,j) = GetEigenvectorFromEigenvalues(H,i,j);
-    end
-end
+ev_of_H = GetAllEigenvectorsFromEigenvalues(H);
 
 disp('Eigenvectors using getEigenvectorFromEigenvalues(H,i,j):')
 disp(ev_of_H);
 
-eigenvectors.^2 - ev_of_H'
+disp('Tolerance for a small Hermitian matrix:')
+disp(eigenvectors.^2 - ev_of_H');
