542600296ead9c79d5134694647f3daa2269b3b8
Chris Gyurgyik <cpg49@cornell.edu>
2019-11-16 17:00:14 -0500

    Added variable number of arguments to allow client to input his or her own eigenvalues for H. This allows for the client to determine best method to compute eigenvalues.

--- GetAllEigenvectorsFromEigenvalues.m
+++ GetAllEigenvectorsFromEigenvalues.m
@@ -1,10 +1,22 @@
 % Iterates through, and produces all the eigenvector values corresponding to
 % 'H'. Stores them into 'ev_of_H'.
-% Requires: 'H' must be a Hermitian matrix.
-function ev_of_H = GetAllEigenvectorsFromEigenvalues(H)
+%   Takes in 1 or 2 arguments:
+%   - 'H' must be a Hermitian matrix.
+%   - (optional) 'H_eigenvalues' are the eigenvalues of H. If not provided,
+%     defaults to using MATLAB's eig().
+function ev_of_H = GetAllEigenvectorsFromEigenvalues(varargin)
+    H = varargin{1};
+    if nargin == 1
+        H_eigenvalues = eig(H);
+    elseif nargin == 2
+        H_eigenvalues = varargin{2};
+    else
+         error('GetEigenvectorFromEigenvalues accepts 1 or 2 arguments.')
+    end
+    
     n = length(H);
     ev_of_H = zeros(n,n);
     for j = 1:n
-        ev_of_H(:,j) = GetEigenvectorFromEigenvalues(H,1:n,j);
+        ev_of_H(:,j) = GetEigenvectorFromEigenvalues(H,1:n,j, H_eigenvalues);
     end
 end
--- GetEigenvectorFromEigenvalues.m
+++ GetEigenvectorFromEigenvalues.m
@@ -1,35 +1,38 @@
 % Calculating eigenvectors from eigenvalues.
-% For more information on proof, see: https://arxiv.org/pdf/1908.03795.pdf
-
-% Gets the normed eigenvector ||v(i,j)||^2 from the matrix H.
-% 'H' must be a Hermitian matrix.
-% 'i' is the row.    1 <= i <= N.
-% 'j' is the column. 1 <= j <= N.
-function v_j = GetEigenvectorFromEigenvalues(H, ii, j)
-     % 1. Calculate the eigenvalues of λi(H).
-     H_eigenvalues = eig(H); 
-     
-     % 2. Calculate eigenvalues of hj 
+% Gets the normed eigenvector ||v(ii,j)||^2 from the matrix H.
+%   Takes in 3 or 4 arguments:
+%   - 'H' must be a Hermitian matrix.
+%   - 'ii' is the row(s) of column 'j'.    for each i, 1 <= i <= N.
+%   - 'j' is the column.                               1 <= j <= N.
+%   - (optional) 'H_eigenvalues' are the eigenvalues of H. If not provided,
+%     defaults to using MATLAB's eig().
+% Currently, the eigenvalues of hj are produced using MATLAB's eig().
+% Requires: 'H_eigenvalues' must have N eigenvalues.
+function v_j = GetEigenvectorFromEigenvalues(varargin)
+     H = varargin{1};
+     ii = varargin{2};
+     j = varargin{3};
+    
+     if nargin == 3
+         % Calculate the eigenvalues of λi(H).
+         H_eigenvalues = eig(H);
+     elseif nargin == 4
+         H_eigenvalues = varargin{4};
+     else
+         error('GetEigenvectorFromEigenvalues accepts 3 or 4 arguments.')
+     end
+    
      H(:, j) = []; % Remove jth column.
      H(j, :) = []; % Remove jth row.
      Hj_eigenvalues = eig(H);
-          
-     % 3a. res_A = res_A * [λi(H) - λk(hj)] from k = 1 to (N - 1).
-     %res_A = prod( ei - hj_eigenvalues );
-     
-     % 3b. res_B = res_B * [λi(H) - λk(H)]  from k = 1 to N with limitation (k =/= i).
-      
-     %res_B = prod( ei - H_eigenvalues );
-     
-     % 4. |v(i,j)|^2 = res_A / res_B.
-     %v_ij = res_A/res_B;
      
      v_j = zeros(numel(ii),1);
+     
      for k = 1:numel(ii)
          ei = H_eigenvalues(ii(k));
          H_ii = H_eigenvalues;
          H_ii(ii(k)) = [];
          
-         v_j(k) = prod( (ei - Hj_eigenvalues)./(ei - H_ii) );    
+         v_j(k) = prod( (ei - Hj_eigenvalues)./(ei - H_ii) );
      end
 end
