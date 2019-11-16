adea1a465af8840cbc27b258569dd5013815baaf
ajt60gaibb <alex.townsend1987@gmail.com>
2019-11-16 12:53:25 -0500

    Huge speed up. No need to compute eigenvalues of Hj separately for each entry of v_j

--- GetAllEigenvectorsFromEigenvalues.m
+++ GetAllEigenvectorsFromEigenvalues.m
@@ -4,9 +4,7 @@
 function ev_of_H = GetAllEigenvectorsFromEigenvalues(H)
     n = length(H);
     ev_of_H = zeros(n,n);
-    for i = 1:n
-        for j = 1:n
-            ev_of_H(i,j) = GetEigenvectorFromEigenvalues(H,i,j);
-        end
+    for j = 1:n
+        ev_of_H(:,j) = GetEigenvectorFromEigenvalues(H,1:n,j);
     end
 end
--- GetEigenvectorFromEigenvalues.m
+++ GetEigenvectorFromEigenvalues.m
@@ -5,26 +5,31 @@
 % 'H' must be a Hermitian matrix.
 % 'i' is the row.    1 <= i <= N.
 % 'j' is the column. 1 <= j <= N.
-function v_ij = GetEigenvectorFromEigenvalues(H, i, j)
+function v_j = GetEigenvectorFromEigenvalues(H, ii, j)
      % 1. Calculate the eigenvalues of λi(H).
-     H_eigenvalues = eig(H);
-     ei = H_eigenvalues(i); 
+     H_eigenvalues = eig(H); 
      
      % 2. Calculate eigenvalues of hj 
-     hj = H;
-     hj(:, j) = []; % Remove jth column.
-     hj(j, :) = []; % Remove jth row.
-     hj_eigenvalues = eig(hj);
+     H(:, j) = []; % Remove jth column.
+     H(j, :) = []; % Remove jth row.
+     Hj_eigenvalues = eig(H);
           
      % 3a. res_A = res_A * [λi(H) - λk(hj)] from k = 1 to (N - 1).
      %res_A = prod( ei - hj_eigenvalues );
      
      % 3b. res_B = res_B * [λi(H) - λk(H)]  from k = 1 to N with limitation (k =/= i).
-     H_eigenvalues(i) = []; 
+      
      %res_B = prod( ei - H_eigenvalues );
      
      % 4. |v(i,j)|^2 = res_A / res_B.
      %v_ij = res_A/res_B;
      
-     v_ij = prod( (ei - hj_eigenvalues)./(ei - H_eigenvalues) ); 
+     v_j = zeros(numel(ii),1);
+     for k = 1:numel(ii)
+         ei = H_eigenvalues(ii(k));
+         H_ii = H_eigenvalues;
+         H_ii(ii(k)) = [];
+         
+         v_j(k) = prod( (ei - Hj_eigenvalues)./(ei - H_ii) );    
+     end
 end
