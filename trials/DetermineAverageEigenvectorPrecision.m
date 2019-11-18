f4c6fc7b6ed3a6c2cab6d67e4f18766a57a21035
Chris Gyurgyik <cpg49@cornell.edu>
2019-11-18 16:20:12 -0500

    Fix variable initialization.

--- trials/DetermineAverageEigenvectorPrecision.m
+++ trials/DetermineAverageEigenvectorPrecision.m
@@ -1,14 +1,14 @@
 function [avg_rel_tol, avg_abs_tol] = DetermineAverageEigenvectorPrecision(N, number_of_trials)
 % Determines the average relative tolerance and average absolute tolerance
-% for eigenvector production across an N x N Hermitian matrix. 
+% for eigenvector production across an N x N Hermitian matrix.
 % To reduce outliers, this is conducted over k trials, where k = number_of_trials.
 % The comparison occurs between MATLAB's eig() and GetEigenvectorFromEigenvalues().
 
-    H = randn(N,N); 
-    H = (H+H')/2; 
+    H = randn(N,N);
+    H = (H+H')/2;
 
-    rel_tol = 0;
-    abs_tol = 0;
+    rel_tol = zeros(N,N);
+    abs_tol = zeros(N,N);
     
     for k = 1:number_of_trials
         [eig_ev, ~] = eig(H);
