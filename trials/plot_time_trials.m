167113dc581573d543d9104cbd3ea4c7f3328d84
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2019-11-18 06:48:29 -0500

    Create plot_time_trials.m

---
+++ trials/plot_time_trials.m
@@ -0,0 +1,17 @@
+function plot_time_trials(plot_title, x_label, y_label, x_axis, eigenvector_function, ev_fn_times, gefe_fn_times, num_trials)
+    close all;
+    figure;
+    p = plot(x_axis, ev_fn_times, x_axis, gefe_fn_times);
+    p(1).LineWidth = 2;
+    p(1).Color = 'r';
+    p(2).LineWidth = 2;
+    p(2).Color = 'b';
+    
+    title(plot_title);
+    xlabel(x_label);
+    ylabel(y_label);
+    legend(func2str(eigenvector_function),'GetEigenvectorFromEigenvalues');
+    txt = {'   Currently does not account for precision.', ['   Number of trials per N: ', num2str(num_trials)]};
+    text(x_axis(2), ev_fn_times(length(ev_fn_times) - 1), txt);
+    saveas(gcf,'TimeTrials.png')
+end
