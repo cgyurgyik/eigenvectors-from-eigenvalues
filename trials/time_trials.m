e251093c69b649ae847e0a43f3aa424c3c35e0be
Chris Gyurgyik <cpg49@cornell.edu>
2019-11-17 19:31:28 -0500

    Added functionality to save times for each trial.

--- trials/time_trials.m
+++ trials/time_trials.m
@@ -1,4 +1,4 @@
-function time_trials(n, num_trials, eigenvector_function)
+function [ev_fn_time, gefe_fn_time] = time_trials(n, num_trials, eigenvector_function)
 % Quick benchmarking function to retrieve a random eigenvector for k consecutive trials with an n x n matrix.
 %
 % 'n' is the size of the Hermitian matrix. n > 0.
@@ -7,11 +7,18 @@ function time_trials(n, num_trials, eigenvector_function)
 % matrix. Assumes that it returns a single argument, a matrix (or vector) of
 % corresponding eigenvalues.
 %
-% Side note: GetEigenvectorFromEigenvalues() produces the normed eigenvector value. This may not
+% Returns: The time required to run all k trials for both the provided function in [ev_fn_time]
+% and GEFE in [gefe_fn_time].
+%
+% NOTE 1: GetEigenvectorFromEigenvalues() produces the normed eigenvector value. This may not
 % always be the case for @eigenvector_function.
 %
-% Example usage: time_trials(100, 10, @eig);
-% TODO(cgyurgyik): Save times to allow for data plots.
+% NOTE 2: Currently disregards precision!
+%
+% Example usage:
+%               n = 10;
+%               number_of_trials = 100;
+%               [y1, y2] = time_trials(n, number_of_trials, @eig);
 
     H = randn(n,n);
     H = (H+H')/2;
@@ -20,25 +27,32 @@ function time_trials(n, num_trials, eigenvector_function)
     fprintf('   1. n = %d\n   2. num_trials = %d\n', n, num_trials);
     disp('------------------------------------');
     
-    fprintf('   Contesting eigenvector function: %s\n', func2str(eigenvector_function));
-    tic;
-    for k = 1:num_trials
-        % TODO(cgyurgyik): Write helper function, use MATLAB's timeit
-        [eigenvectors, ~] = eigenvector_function(H);
-        eig_rand_eigenvector = eigenvectors(:, randi([1 n]));
-    end
-    toc;
+    fprintf('   Computing contesting eigenvector function: %s \n', func2str(eigenvector_function));
+    ev_fn = @() run_trials_eigenvector_function(H, n, num_trials, eigenvector_function);
+    ev_fn_time = timeit(ev_fn);
     
     disp('------------------------------------');
     
-    fprintf('   GetEigenvectorFromEigenvalues\n');
-    tic;
+    fprintf('   Computing GetEigenvectorFromEigenvalues \n');
+    gefe_fn = @() run_trials_gefe_function(H, n, num_trials);
+    gefe_fn_time = timeit(gefe_fn);
+    
+    disp('-----------------------------------------------------');
+end
+
+% TODO(cgyuryik): documentation.
+function run_trials_eigenvector_function(H, n, num_trials, eigenvector_function)
+    for k = 1:num_trials
+        [eigenvectors, ~] = eigenvector_function(H);
+        eig_rand_eigenvector = eigenvectors(:, randi([1 n]));
+    end
+end
+
+% TODO(cgyuryik): documentation.
+function run_trials_gefe_function(H, n, num_trials)
     eigvals = eig(H);
     ii = 1:n;
     for k = 1:num_trials
         gefe_rand_eigenvector = GetEigenvectorFromEigenvalues(H, ii, randi([1 n]), eigvals);
     end
-    toc;
-    
-    disp('-----------------------------------------------------');
 end
