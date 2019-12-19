24cdc5569c61e0cb658de82caab68007506d56de
Chris Gyurgyik <gyurgyikcp@gmail.com>
2019-12-19 16:28:00 -0500

    cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,11 +3,11 @@
 #include "GetEigenvectorFromEigenvalues.hpp"
 
 // Demonstration.
+int main() {
+    arma::mat H("1 1 ; 0 0");
+    arma::vec ii("2 1");
+    arma::vec eigvals = GetEigenvectorFromEigenvalues(H, ii, /*column j=*/1);
 
-arma::mat H("1 1 ; 0 0");
-arma::vec ii("2 1");
-arma::vec eigvals = GetEigenvectorFromEigenvalues(H, ii, /*column j=*/1);
-
-eigvals.print(); // "0 1"
-
+    eigvals.print(); // "0 1"
+}
 
