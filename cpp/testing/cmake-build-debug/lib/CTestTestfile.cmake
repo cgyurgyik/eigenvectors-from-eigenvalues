e843d8849bd44579d326593c2c8ac60b9226215d
Chris Gyurgyik <gyurgyikcp@gmail.com>
2019-12-23 08:08:08 -0500

    Removed II.

--- cpp/testing/cmake-build-debug/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/cgyurgyik/CLionProjects/gefe/eigenvectors-from-eigenvalues/cpp/testing/lib
-# Build directory: /Users/cgyurgyik/CLionProjects/gefe/eigenvectors-from-eigenvalues/cpp/testing/cmake-build-debug/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
--- cpp/testing/cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/cgyurgyik/CLionProjects/gefe/eigenvectors-from-eigenvalues/cpp/testing/lib/googletest/googletest
-# Build directory: /Users/cgyurgyik/CLionProjects/gefe/eigenvectors-from-eigenvalues/cpp/testing/cmake-build-debug/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
