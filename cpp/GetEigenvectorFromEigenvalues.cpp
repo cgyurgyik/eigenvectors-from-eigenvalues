61d4cd915e3538acc2832d14cec8a6583331b904
Chris Gyurgyik <gyurgyikcp@gmail.com>
2019-12-23 07:10:52 -0500

    Implemented for i and ii.

--- cpp/GetEigenvectorFromEigenvalues.cpp
+++ cpp/GetEigenvectorFromEigenvalues.cpp
@@ -1,6 +1,29 @@
 #include "GetEigenvectorFromEigenvalues.h"
 #include <armadillo>
 
+// Given the corresponding eigenvalues of H and H with the jth column removed, returns the ith
+// row eigenvector. This is denoted as:
+// prod(lambda_H_i - lambda_Hj_k) / prod(lambda_H_i - lambda_H_k), when i != k.
+double get_rowi_eigenvector(int i, const arma::mat& H_eigenvalues, const arma::mat& Hj_eigenvalues) {
+    const double lambda_H_i = H_eigenvalues[i];
+    double v_ij = 1;
+    const int jth_column = H_eigenvalues.size() - 1;
+
+    for (int k = 0; k < H_eigenvalues.size(); ++k) {
+        double numerator = 1;   // lambda_H_i - lambda_Hj_k
+        double denominator = 1; // lambda_H_i - lambda_H_k
+        if (k != jth_column) {
+            numerator = lambda_H_i - Hj_eigenvalues[k];
+        }
+        const double lambda_H_k = H_eigenvalues[k];
+        if (lambda_H_k != lambda_H_i) {
+            denominator = lambda_H_i - lambda_H_k;
+        }
+        v_ij *= numerator/denominator;
+    }
+    return v_ij;
+}
+
 double GetEigenvectorFromEigenvalues(const arma::mat& H, int i, int j) {
     if (!H.is_square()) {
         throw std::invalid_argument("\nH is not square.");
@@ -16,28 +39,40 @@ double GetEigenvectorFromEigenvalues(const arma::mat& H, int i, int j) {
                                     "such that 0 <= j <= N-1.");
     }
 
-    // 1.  Produce eigenvalues of H.
     const arma::vec H_eigenvalues = eig_sym(H);
 
-    // 2a. Remove jth row and column
-    arma::mat Hj = H;
-    Hj.shed_col(j);
-    Hj.shed_row(j);
+    arma::mat Hj_eigenvalues = H;
+    Hj_eigenvalues.shed_col(j);
+    Hj_eigenvalues.shed_row(j);
 
-    // 2b. Produce eigenvalues of Hj.
-    const arma::vec Hj_eigenvalues = eig_sym(Hj);
-    const double lambda_H_i = H_eigenvalues[i];
-    double v_ij = 1;
+    return get_rowi_eigenvector(i, H_eigenvalues, Hj_eigenvalues);
+}
 
-    for (int k = 0; k < Hj_eigenvalues.size(); ++k) {
-        const double lambda_Hj_k = Hj_eigenvalues[k];
-        v_ij *= (lambda_H_i - lambda_Hj_k);
+arma::vec GetEigenvectorFromEigenvalues(const arma::mat& H, const arma::vec& ii, int j) {
+    if (!H.is_square()) {
+        throw std::invalid_argument("\nH is not square.");
+    }
+    for (const double i : ii) {
+        if (i < 0 || i > H.n_rows - 1) {
+            throw std::invalid_argument("\nEach i in ii must be a row of the matrix H. "
+                                        "For each i, 0 <= i <= N-1.");
+        }
+    }
+    if (j < 0 || j > H.n_cols - 1) {
+        throw std::invalid_argument("\nj must be an integer representing the "
+                                    "column of the desired eigenvector values, "
+                                    "such that 0 <= j <= N-1.");
     }
 
-    for (int l = 0; l < H_eigenvalues.size(); ++l) {
-        const double lambda_H_k = H_eigenvalues[l];
-        if (lambda_H_k == lambda_H_i) continue;
-        v_ij *= (1 / (lambda_H_i - lambda_H_k));
+    const arma::vec H_eigenvalues = eig_sym(H);
+
+    arma::mat Hj_eigenvalues = H;
+    Hj_eigenvalues.shed_col(j);
+    Hj_eigenvalues.shed_row(j);
+
+    arma::vec v_ij(ii.size(), arma::fill::zeros);
+    for (const double i : ii) {
+        v_ij[i] = get_rowi_eigenvector(i, H_eigenvalues, Hj_eigenvalues);
     }
     return v_ij;
 }
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,15 +4,19 @@
 // Demonstration.
 int main() {
     const arma::mat H("-0.2414 0.3160; 0.3160 -0.8649");
-    const double eigvec_ij = GetEigenvectorFromEigenvalues(H, /*row i=*/0, /*column j=*/0);
+    const arma::vec ii("0 1");
+    const arma::vec gefe_eigvecs = GetEigenvectorFromEigenvalues(H, ii, /*column j=*/0);
+    printf("\ngefe:\n");
+    gefe_eigvecs.print(); //    0.1488
+                          //    0.8512
 
-    printf("\nGEFE: %f\n", eigvec_ij);
     arma::vec arma_eigvals;
     arma::mat arma_eigvecs;
     arma::eig_sym(arma_eigvals, arma_eigvecs, H);
 
     printf("\narma::eig_sym:\n");
     arma_eigvecs = arma::square(arma_eigvecs);
-    arma_eigvecs.col(0).print();
+    arma_eigvecs.col(0).print(); //    0.1488
+                                         //    0.8512
 }
 
